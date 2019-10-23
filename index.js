1dded6fe275262a54270eb1750ece05e95967d0f
Belle P <bellepx3@gmail.com>
2019-10-23 18:23:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
 
 
+<<<<<<< HEAD
 const companyName = 'Scuber'
 
 let mostProfitableNeighborhood = "Chelsea"
 
 let companyCeo = "Susan Smith"
+=======
+const companyName = 'Scuber'
+>>>>>>> 68be3958b8a0cda81b80572418010af787755658
